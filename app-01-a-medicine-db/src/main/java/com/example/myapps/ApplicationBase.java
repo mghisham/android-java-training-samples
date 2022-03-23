f65c936add67ee44d56df11648343238fe773f49
suhailkk1991 <suhailkk1991@gmail.com>
2022-03-23 20:59:50 +0530

    Split App01 into two,App-01a and app0b for data base connection and alarm manager

--- app-01-a-medicine-db/src/main/java/com/example/myapps/ApplicationBase.java
+++ app-01-a-medicine-db/src/main/java/com/example/myapps/ApplicationBase.java
@@ -2,8 +2,6 @@ package com.example.myapps;
 
 import android.app.Application;
 
-import com.facebook.stetho.Stetho;
-
 public class ApplicationBase extends Application {
     public void onCreate() {
         super.onCreate();
