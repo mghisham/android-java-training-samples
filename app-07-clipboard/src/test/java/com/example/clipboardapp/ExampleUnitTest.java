8c8c472e1a24fc53df989cd0ad9d8d8ac396b62b
Hisham <mghisham@gmail.com>
2022-03-25 12:32:35 +0530

    Clipboard app created
    * Screenshots added

--- app-07-clipboard/src/test/java/com/example/clipboardapp/ExampleUnitTest.java
+++ app-07-clipboard/src/test/java/com/example/clipboardapp/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.example.app_07_clipboard;
+package com.example.clipboardapp;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
